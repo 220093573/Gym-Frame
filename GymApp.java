6d275661662dc59c0e85c01235e6008a6203cdf8
220093573 <140013778+220093573@users.noreply.github.com>
2023-07-26 13:03:52 +0200

    Add files via upload

---
+++ GymApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gymapp;
+
+import za.ac.tut.ui.GymMembershipFrame;
+
+/**
+ *
+ * @author LISWO
+ */
+public class GymApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GymMembershipFrame gymFrame;
+        gymFrame = new GymMembershipFrame();
+    }
+    
+}
